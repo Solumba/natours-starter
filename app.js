f9fd3503452966dc6ece6b1f495c2a847b4c1986
Soloumba <olumbasolomon08@gmail.com>
2022-08-09 11:23:19 +0100

    Route handling

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
 
-app.get('/', (req, res) =>
-  res.status(200).json({ message: 'hello from the server' })
+//this function can run synchronously because we only need to get all the tours once
+//you should not run synchronouse codes in your routes
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
 );
-app.post('/api', (req, res) =>
-  res.status(200).send('you can post to this endpoint...')
-);
+console.log(tours);
+
+//route handling
+app.get('/', (req, res) => {
+  res.header({ 'Content-Type': 'text/HTML' });
+  res.status(200).send('<h1>Welcome to Natours</h1>');
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
+});
 
 const port = 3000;
-app.listen(port, () => console.log('App Running'));
-
-// const http = require('http');
-// const server = http.createServer((req, res) => {
-//     if(req.url === '/'){
-//         res.end("hello world");
-//     }
-// })
-
-// server.listen(3000, '127.0.0.1', ()=> console.log("server running"))
+app.listen(port, () => console.log(`App running at http://127.0.0.1:${port}`));
