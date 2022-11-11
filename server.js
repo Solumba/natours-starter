4869d3659607e7323d052f2bc931f21bac37fc57
Soloumba <olumbasolomon08@gmail.com>
2022-11-11 21:26:41 +0100

    creating a mongo DB schema and model

--- server.js
+++ server.js
@@ -6,11 +6,34 @@ const DB = process.env.DATABASE;
 
 const app = require('./app');
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  // eslint-disable-next-line no-console
+  .then(() => console.log('Database Connection Successful'));
+
+//Create Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'A name is required'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A rating is required'],
+  },
 });
+
+//Create Model
+const Tours = mongoose.model('Tours', tourSchema);
 
 //Start Server
 const port = 3000;
